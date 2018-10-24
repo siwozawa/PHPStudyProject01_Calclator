8126d9a35bdf1a394af83c9049dd2b2827796d99
siwozawa <37283298+siwozawa@users.noreply.github.com>
2018-10-25 02:33:32 +0900

    20181024_01
    日付変更後の更新になってしまった。
    HTML のおかしなところを修正して、
    計算結果を即座に表示する欄を設けた。
    内部ロジックにも着手を始める。

---
+++ calc.php
@@ -0,0 +1,44 @@
+
+<?php
+    $pushedBtn = $_POST['btn'] ;
+
+    echo '押されたボタンは' . $pushedBtn . 'です' . "<br />\n" ;
+
+    // switch($pushedBtn) {
+    //     case "0" :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    //     case ( $pushedBtn === 1"" ) :
+    //         break ;
+    // }
+
+?>
